eab03778860a7bb0643629d6fc5e9b4a9632e319
Oren <orengolan@gmail.com>
2019-08-22 17:05:26 -0700

    Updates

---
+++ precache-manifest.14f8549aae25ed0402d85481bd0d5f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad08ff0c5d8ae275655aa89fc726a615",
+    "url": "/food/index.html"
+  },
+  {
+    "revision": "b0881b7833794daf4124",
+    "url": "/food/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4edbb2967e3d2bd8f3e3",
+    "url": "/food/static/js/2.868c74c1.chunk.js"
+  },
+  {
+    "revision": "b0881b7833794daf4124",
+    "url": "/food/static/js/main.90dda028.chunk.js"
+  },
+  {
+    "revision": "af25f268c510ce789f44",
+    "url": "/food/static/js/runtime~main.0bf829c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/food/static/media/logo.5d5d9eef.svg"
+  }
+]);
